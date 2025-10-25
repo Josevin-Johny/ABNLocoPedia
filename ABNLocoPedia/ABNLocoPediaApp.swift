80cdd325e32b5fb07861c0dab2b258ea68b578f9
Josevin <josevin.nl1988@gmail.com>
2025-10-25 20:34:15 +0200

    Initial Commit

---
+++ ABNLocoPedia/ABNLocoPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABNLocoPediaApp.swift
+//  ABNLocoPedia
+//
+//  Created by Josevin Johny on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ABNLocoPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
